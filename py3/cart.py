61217e9b6766233e5e9da43027766b330722e596
FluidSense <22146437+FluidSense@users.noreply.github.com>
2018-12-28 15:51:03 +0100

    add cart, item

---
+++ py3/cart.py
@@ -0,0 +1,17 @@
+
+class Cart:
+    def __init__(self):
+        self.inventory = []
+
+    def add(self, item):
+        self.inventory.add(item)
+
+class Item:
+    def __init__(self, price, name):
+        self.price = price
+        self.name = name
+        self.quantity = 0
+
+    def addOne(self):
+        self.quantity += 1
+
