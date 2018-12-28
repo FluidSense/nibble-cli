59d44423e9b399a5d60702a86a79a3b0595dbb0e
FluidSense <22146437+FluidSense@users.noreply.github.com>
2018-12-28 11:01:57 +0100

    get purchasable items

---
+++ py3/list.py
@@ -0,0 +1,7 @@
+import requests, json
+
+store = requests.get("https://online.ntnu.no/api/v1/inventory")
+store = json.loads(store.text) if store.status_code == 200 else print("Error fetching inventoryi: " + str(store.status_code))
+priceList = {k["name"]:k["price"] for k in store}
+for k,v in priceList.items():
+    print(k, v)
